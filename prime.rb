df60729076708b4f3b75e0d247d5056db193e93c
Richard Kim <richeekim@gmail.com>
2018-03-29 18:48:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+
+  x = 2
+
+  while x < number
+    if number % x == 0
+      return false
+    end
+    x += 1
+  end
+  return true
+end
